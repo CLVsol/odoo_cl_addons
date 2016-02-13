354558143a3b8fa4df9ead27ee4c930cab58a07f
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2016-02-13 10:19:39 -0200

    Updated module "cl_place" (Added place category).

--- cl_place/__openerp__.py
+++ cl_place/__openerp__.py
@@ -35,7 +35,7 @@
         'security/cl_place_security.xml',
         # 'security/ir.model.access.csv',
         'cl_place_view.xml',
-        # 'category/cl_place_category_view.xml',
+        'category/cl_place_category_view.xml',
         # 'tag/cl_tag_view.xml',
         # 'annotation/cl_annotation_view.xml',
         # 'seq/cl_place_seq_view.xml',
